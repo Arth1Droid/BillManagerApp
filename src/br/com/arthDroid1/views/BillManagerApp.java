5cffdfa3cbaf10ce876efbc8face6fc52b4da9a3
ArthDroid1 <barbosarthur7@gmail.com>
2025-11-09 20:08:06 -0300

    create class BillManagerApp

---
+++ src/br/com/arthDroid1/views/BillManagerApp.java
@@ -0,0 +1,12 @@
+package com.arthDroid1.views;
+
+import com.arthDroid1.controllers.BillController;
+
+public class BillManagerApp {
+    
+    public static void main(String[] args) {
+        BillController controller = new BillController();
+
+        controller.startMenu();
+    }
+}
