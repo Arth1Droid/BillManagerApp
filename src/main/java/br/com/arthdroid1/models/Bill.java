7c6247e6c2bff4b5ff476774fea08a52286076d8
ArthDroid1 <barbosarthur7@gmail.com>
2025-11-14 17:08:47 -0300

    Start springboot version

---
+++ src/main/java/br/com/arthdroid1/models/Bill.java
@@ -0,0 +1,129 @@
+package br.com.arthdroid1.models;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+public class Bill {
+    private UUID id;
+    private String description;
+    private LocalDate payDay;
+    private LocalDate dueDate;
+    private BillStatus status;
+    private BillType billType;
+    private double cost;
+
+    public Bill(BillType billType,double cost, String description, LocalDate dueDate){
+       
+        if (description.isBlank()) {
+            throw new IllegalArgumentException("The description cannt be blank");
+        }
+        if (cost < 0) {
+            throw new IllegalArgumentException("The cost cannot be 0 or negative");       
+        }  
+
+        if (dueDate == null){
+            throw new IllegalArgumentException("Due date cannot be null.");
+        }
+
+        this.cost = cost;
+        this.id = UUID.randomUUID();
+        this.dueDate = dueDate;
+        this.billType = billType;
+        this.description = description;
+        this.payDay = null;
+        this.status = BillStatus.OPEN;
+    }
+
+    public void payBill(LocalDate paymentDateTime){
+        if (!(status == BillStatus.OPEN || status ==  BillStatus.OVERDUE)) {
+            throw new IllegalArgumentException("The bill is already paid");
+        }
+        if (paymentDateTime == null) {
+            throw new IllegalArgumentException("The payment date/time cannot be null.");
+        }
+        
+        this.payDay = paymentDateTime;
+        this.status = BillStatus.PAID;
+    }
+
+    public void verifyDueDate(LocalDate today){
+        if (today.isAfter(dueDate) && status == BillStatus.OPEN) {
+            status = BillStatus.OVERDUE;
+        }
+    }
+
+    public Long remainingDays(LocalDate today){
+        if (status == BillStatus.OVERDUE) {
+            return 0L;
+        }
+
+        Long daysBetweenDates = ChronoUnit.DAYS.between(today, dueDate);
+
+        return Math.max(0L, daysBetweenDates);
+
+    }
+
+    public boolean isOverdue(){
+        return status == BillStatus.OVERDUE;
+    }
+
+    public double getCost(){
+        return cost;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getPayDay() {
+        return payDay;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public BillStatus getStatus() {
+        return status;
+    }
+
+    public BillType getBillType() {
+        return billType;
+    }
+
+    public void setDescription(String description) {
+        
+        if (description == null || description.isBlank()) {
+             throw new IllegalArgumentException("The description cannot be blank.");
+    }
+        this.description = description;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        if (dueDate.isBefore(LocalDate.now())) {
+             throw new IllegalArgumentException("The due date cannot be before today");
+        }
+        this.dueDate = dueDate;
+    } 
+
+    public void setStatus(BillStatus status){
+        this.status = status;
+    }
+    public void setBillType(BillType billType){
+        this.billType = billType;
+    }
+
+    public void setPayDay(LocalDate payDay) {
+        this.payDay = payDay;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+}
---
+++ src/main/java/br/com/arthdroid1/models/BillStatus.java
@@ -0,0 +1,29 @@
+package br.com.arthdroid1.models;
+
+public enum BillStatus {
+    
+    OPEN ("Open"),
+    PAID("Paid"),
+    OVERDUE("Overdue");
+
+    private String description;
+    BillStatus(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+        public static BillStatus fromDescription(String description) {
+        if (description == null) {
+            return null; 
+        }
+        for (BillStatus type : BillStatus.values()) {
+            if (type.description.equalsIgnoreCase(description.trim())) {
+                return type; 
+            }
+        }
+        return null; 
+    }
+}
