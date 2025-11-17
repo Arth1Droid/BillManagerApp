1c36ab323dbdbbfde0adc2e1a5705887abdb1cc3
ArthDroid1 <barbosarthur7@gmail.com>
2025-11-17 19:31:35 -0300

    Fix bugs

---
+++ src/main/java/br/com/arthdroid1/BillManagerApp.java
@@ -0,0 +1,11 @@
+package br.com.arthdroid1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillManagerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BillManagerApp.class, args);
+    }
+}
--- src/main/java/br/com/arthdroid1/models/Bill.java
+++ src/main/java/br/com/arthdroid1/models/Bill.java
@@ -16,7 +16,7 @@ import jakarta.persistence.Table;
 @Table(name = "bill")
 public class Bill {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name ="description")
     private String description;
