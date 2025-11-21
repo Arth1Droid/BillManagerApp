9f2998307d8fc9a7d1b8640f398c6a135035a745
ArthDroid1 <barbosarthur7@gmail.com>
2025-11-20 23:51:42 -0300

    Add inital configs in SecurityConfig class

--- src/main/java/br/com/arthdroid1/security/SecurityConfig.java
+++ src/main/java/br/com/arthdroid1/security/SecurityConfig.java
@@ -1,5 +1,24 @@
 package br.com.arthdroid1.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+		.authorizeHttpRequests(auth ->{
+			auth.requestMatchers("/users/").permitAll()
+			.requestMatchers("/bills/").permitAll();
+			auth.anyRequest().authenticated();
+		});
+		
+		return http.build();
+		
+	}
 
 }
